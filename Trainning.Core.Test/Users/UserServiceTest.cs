68de4b190b7f0350538f1b66650c5d23a0f396db
Daniel Yepes <arquitecto@sypsolutions.com>
2022-12-16 18:03:01 -0500

    fix test execution

--- Trainning.Core.Test/Users/UserServiceTest.cs
+++ Trainning.Core.Test/Users/UserServiceTest.cs
@@ -11,19 +11,10 @@ public class UnitTest1
     public void TestMethod1()
     {
         var services = new ServiceCollection();
-        services.AddTransient<IUserService, UserService>();
-        services.AddSingleton<IMailSender, FakeMailSender>();
+        services.ConfigureUserService();
         var serviceProvider = services.BuildServiceProvider();
         
         var userService = serviceProvider.GetRequiredService<IUserService>();
-        userService.CreateUser(new UserDto());
-    }
-}
-
-public class FakeMailSender : IMailSender
-{
-    public bool SendEmail(string email, string message)
-    {
-        return true;
+        Assert.ThrowsException<NotImplementedException>(() => userService.CreateUser(new UserDto()));
     }
 }
